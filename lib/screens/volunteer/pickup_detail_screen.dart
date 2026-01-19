9b809c50d45ddef6dc22973b4f3764a204e46bb2
AngelinFemi <145834515+AngelinFemi@users.noreply.github.com>
2026-01-19 16:41:46 +1100

    Update pickup_detail_screen.dart

--- lib/screens/volunteer/pickup_detail_screen.dart
+++ lib/screens/volunteer/pickup_detail_screen.dart
@@ -1,1 +1,30 @@
-// Pickup Detail Screen
+import 'package:flutter/material.dart';
+
+class PickupDetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Pickup Details')),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Veg Biryani', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+            SizedBox(height: 10),
+            Text('Quantity: 50 servings'),
+            Text('Location: City Hall'),
+            Text('Posted: 10:00 AM'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // TODO: Start Navigation to Donor
+              },
+              child: Text('Start Pickup'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
