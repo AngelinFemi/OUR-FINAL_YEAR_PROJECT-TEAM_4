36605004887cb4448c34ba182a10f5f9b206ac20
AngelinFemi <145834515+AngelinFemi@users.noreply.github.com>
2026-01-19 16:42:05 +1100

    Update delivery_tracking_screen.dart

--- lib/screens/volunteer/delivery_tracking_screen.dart
+++ lib/screens/volunteer/delivery_tracking_screen.dart
@@ -1,1 +1,16 @@
-// Delivery Tracking Screen
+import 'package:flutter/material.dart';
+
+class DeliveryTrackingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Delivery Tracking')),
+      body: Center(
+        child: Text(
+          'Map Placeholder for live tracking',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
