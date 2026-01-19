d7196f686dc72165d143cfcc8d8da0d563175559
AngelinFemi <145834515+AngelinFemi@users.noreply.github.com>
2026-01-19 16:34:52 +1100

    Update user_model.dart

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,2 +1,9 @@
-// User model
-class UserModel {}
+class UserModel {
+  String? uid;
+  String? name;
+  String? email;
+  String? role;
+  String? location;
+
+  UserModel({this.uid, this.name, this.email, this.role, this.location});
+}
