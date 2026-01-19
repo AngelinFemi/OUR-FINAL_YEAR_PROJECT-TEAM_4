cdc1b5bad7964f67b677a37fa96b3913645438f3
AngelinFemi <145834515+AngelinFemi@users.noreply.github.com>
2026-01-19 16:34:05 +1100

    Update location_screen.dart

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,1 +1,37 @@
-// Location Screen
+import 'package:flutter/material.dart';
+
+class LocationScreen extends StatelessWidget {
+  final String role;
+  LocationScreen({required this.role});
+
+  final TextEditingController locationController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Enter Your Location')),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: locationController,
+              decoration: InputDecoration(labelText: 'Enter your city or pin code'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                if (role == 'Donor') {
+                  // TODO: Navigate to Donor Home
+                } else {
+                  // TODO: Navigate to Volunteer Home
+                }
+              },
+              child: Text('Proceed'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
