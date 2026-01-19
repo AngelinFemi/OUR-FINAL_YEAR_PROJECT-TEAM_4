d94e94ed82d29742f214b228b2e028702aa77712
AngelinFemi <145834515+AngelinFemi@users.noreply.github.com>
2026-01-19 16:46:39 +1100

    Update location_service.dart

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -1,1 +1,22 @@
-// Location Service
+import 'package:location/location.dart';
+
+class LocationService {
+  final Location location = Location();
+
+  Future<Map<String, double>?> getCurrentLocation() async {
+    bool serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) return null;
+    }
+
+    PermissionStatus permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) return null;
+    }
+
+    LocationData data = await location.getLocation();
+    return {"latitude": data.latitude!, "longitude": data.longitude!};
+  }
+}
