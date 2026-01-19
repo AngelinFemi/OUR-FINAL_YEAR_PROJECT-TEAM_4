802adfcaa184cf64c07413a47839ef9682413c0c
AngelinFemi <145834515+AngelinFemi@users.noreply.github.com>
2026-01-19 16:32:30 +1100

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,1 +1,30 @@
-// Splash Screen
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (_) => LoginScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Food Donation App',
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.green),
+        ),
+      ),
+    );
+  }
+}
