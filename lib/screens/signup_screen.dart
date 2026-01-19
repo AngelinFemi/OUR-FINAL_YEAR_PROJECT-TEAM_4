56c658aca6e1360865d0a1d82bd0bba865d95c1f
AngelinFemi <145834515+AngelinFemi@users.noreply.github.com>
2026-01-19 16:33:25 +1100

    Update signup_screen.dart

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,1 +1,40 @@
-// Signup Screen
+import 'package:flutter/material.dart';
+import 'role_selection_screen.dart';
+
+class SignupScreen extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Signup', style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+            SizedBox(height: 30),
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // TODO: Firebase signup
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => RoleSelectionScreen()));
+              },
+              child: Text('Signup'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
