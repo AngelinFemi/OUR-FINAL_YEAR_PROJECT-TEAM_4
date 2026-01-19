784ce70dbeea6b2af6b9761643d2c498539ded9e
AngelinFemi <145834515+AngelinFemi@users.noreply.github.com>
2026-01-19 16:35:14 +1100

    Update food_model.dart

--- lib/models/food_model.dart
+++ lib/models/food_model.dart
@@ -1,2 +1,10 @@
-// Food model
-class FoodModel {}
+class FoodModel {
+  String? id;
+  String? donorId;
+  String? foodType;
+  int? quantity;
+  String? location;
+  DateTime? expiryTime;
+
+  FoodModel({this.id, this.donorId, this.foodType, this.quantity, this.location, this.expiryTime});
+}
