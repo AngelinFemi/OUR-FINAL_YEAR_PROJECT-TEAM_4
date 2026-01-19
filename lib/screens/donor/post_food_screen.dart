1a3fae1d27c073e112a756df2b43a6f953e23b8b
AngelinFemi <145834515+AngelinFemi@users.noreply.github.com>
2026-01-19 16:47:30 +1100

    Update post_food_screen.dart

--- lib/screens/donor/post_food_screen.dart
+++ lib/screens/donor/post_food_screen.dart
@@ -1,40 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import '../services/location_service.dart';
 
-class PostFoodScreen extends StatelessWidget {
+class PostFoodScreen extends StatefulWidget {
+  @override
+  _PostFoodScreenState createState() => _PostFoodScreenState();
+}
+
+class _PostFoodScreenState extends State<PostFoodScreen> {
+  LatLng? _selectedLocation;
   final TextEditingController foodController = TextEditingController();
   final TextEditingController quantityController = TextEditingController();
-  final TextEditingController locationController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  void _getCurrentLocation() async {
+    var loc = await LocationService().getCurrentLocation();
+    if (loc != null) {
+      setState(() {
+        _selectedLocation = LatLng(loc["latitude"]!, loc["longitude"]!);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Post Food')),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          children: [
-            TextField(
-              controller: foodController,
-              decoration: InputDecoration(labelText: 'Food Name'),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: [
+                TextField(
+                  controller: foodController,
+                  decoration: InputDecoration(labelText: 'Food Name'),
+                ),
+                TextField(
+                  controller: quantityController,
+                  decoration: InputDecoration(labelText: 'Quantity'),
+                  keyboardType: TextInputType.number,
+                ),
+              ],
             ),
-            TextField(
-              controller: quantityController,
-              decoration: InputDecoration(labelText: 'Quantity (servings)'),
-              keyboardType: TextInputType.number,
-            ),
-            TextField(
-              controller: locationController,
-              decoration: InputDecoration(labelText: 'Location'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // TODO: Add Firebase Save Logic
-              },
-              child: Text('Post Food'),
-            )
-          ],
-        ),
+          ),
+          Expanded(
+            child: _selectedLocation == null
+                ? Center(child: CircularProgressIndicator())
+                : FlutterMap(
+                    options: MapOptions(
+                      center: _selectedLocation!,
+                      zoom: 16.0,
+                      onTap: (tapPos, point) {
+                        setState(() {
+                          _selectedLocation = point;
+                        });
+                      },
+                    ),
+                    children: [
+                      TileLayer(
+                        urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                        subdomains: ['a', 'b', 'c'],
+                      ),
+                      MarkerLayer(
+                        markers: [
+                          Marker(
+                            width: 80,
+                            height: 80,
+                            point: _selectedLocation!,
+                            builder: (ctx) => Icon(Icons.location_on, color: Colors.red, size: 40),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // TODO: Save food + location to Firebase
+            },
+            child: Text('Post Food'),
+          ),
+        ],
       ),
     );
   }
